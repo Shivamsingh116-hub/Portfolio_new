3e5ad1cbb7de3491d21bbeb5cd9f800e07a28661
Shivamsingh116-hub <shivamsinghk116@gmail.com>
2026-01-16 20:34:22 +0530

    update current work

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import profileImg from '../assets/profileImg.png'
+import profileImg from '../assets/profileImg.webp'
 const Home = () => {
     return (
-        <div className='bg-red-50 min-h-screen flex min-w-screen pt-24 px-14'>
+        <div className='bg-blue-50
+ min-h-screen flex min-w-screen pt-24 px-14'>
             <section className='flex  text-[1.4rem] flex-col p-10
              gap-2 md:w-1/2 w-full '>
                 <h3
@@ -30,11 +31,14 @@ const Home = () => {
             </section>
             <section className=' md:w-1/2 flex justify-center  w-full'>
                 <div className='md:w-96 w-40 md:h-96 h-40  overflow-hidden  
-                rounded-full shadow-lg  mt-20 bg-gradient-to-br from-slate-100 to-slate-200 '>
+                rounded-full  mt-20 
+                shadow-[0_4px_16px_rgba(0,0,0,0.15)]
+                bg-linear-to-br from-slate-200 to-blue-200 
+                '>
                     <img
                         className='w-full h-full object-cover'
                         src={profileImg}
-                        loading="lazy"
+                        loading="eager"
                         decoding="async"
                         alt='img' />
                 </div>
